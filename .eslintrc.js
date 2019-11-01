9557584065d2d4931ede119a6c16a689c8050ebe
chethan <chethan.np@bigbasket.com>
2019-11-01 19:33:16 +0530

    Added eslint, prettier and pre-commit hook to the project

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+      browser: true,
+      es6: true,
+      node: true,
+      commonjs: true
+  },
+  extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jest/recommended"
+  ],
+  globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+      process: true,
+      __isBrowser__: true
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+      ecmaFeatures: {
+          jsx: true
+      },
+      ecmaVersion: 2015,
+      sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+      indent: ["error", 4],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"]
+  }
+};
