a4ed73d6d268ae084aaeb1976656df288057fe4e
chethan <chethan.np@bigbasket.com>
2019-11-02 10:39:05 +0530

    Code refractor

---
+++ src/server/helpers/renderer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Home from "../../client/components/Home";
+
+export default (req, res) => {
+  const content = renderToString(<Home />);
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Casaone - Shopping Cart</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no, user-scalable=yes">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="/images/favicon.ico">  
+        <script src="bundle.js" defer></script>    
+    </head>
+    <body>
+        <div id="root">${content}</div>
+    </body>
+    </html>
+  `;
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,12 @@
 import express from "express";
-import React from "react";
-import { renderToString } from "react-dom/server";
-import Home from "../client/components/Home";
+import renderer from "./helpers/renderer";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const content = renderToString(<Home />);
-
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Casaone - Shopping Cart</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no, user-scalable=yes">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="shortcut icon" href="/images/favicon.ico">  
-        <script src="bundle.js" defer></script>    
-    </head>
-    <body>
-        <div id="root">${content}</div>
-    </body>
-    </html>
-  `;
-  res.send(html);
+  res.send(renderer());
 });
 
 app.listen(9000, () => {
